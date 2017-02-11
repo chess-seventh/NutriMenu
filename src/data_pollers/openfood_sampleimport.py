c1c876d291841221bb8be883c3e66746a1ee30c2
nevr0sed <piva.francesco@gmail.com>
2017-02-11 09:58:18 +0100

    screenshots from kevin

--- src/data_pollers/openfood_sampleimport.py
+++ src/data_pollers/openfood_sampleimport.py
@@ -1,13 +1,33 @@
+"""
+Sample Python 2.7 code for a call against the OpenFood API product search
+Replace [API_KEY] with your API Key
+curl -X POST --header "Content-Type: application/vnd.api+json" --header "Accept: application/json" --header "Authorization: Token token=[API_KEY]" -d "{
+  \"query\": {
+    \"wildcard\": {
+      \"name_translations.en\" : \"toblerone*\"
+    }
+  }
+}" "https://www.openfood.ch/api/v2/products/_search"
+USAGE:
+$ python product_search.py
+MORE INFO:
+For more examples of search queries using Elastic Search's DSL, see the curl examples here:
+https://github.com/salathegroup/openfood_api/blob/master/sample_code/curl/openfood_api.md
+"""
+
 import requests
 
 BASE_URL='https://www.openfood.ch/api/v2'
 API_KEY='7bae318b548c33fb739bf02ce2636072'
 
-url = BASE_URL + '/products'
+url = BASE_URL + '/products/_search'
 
-query = {
-  "page[number]": "2",
-  "size[size]": "5"
+query =   {
+  "query": {
+    "wildcard": {
+      "name_translations.en" : "tomato"
+    }
+  }
 }
 
 headers = {
@@ -16,6 +36,6 @@ headers = {
   'Content-Type': 'application/vnd.api+json'
 }
 
-r = requests.get(url, params=query, headers=headers)
+r = requests.post(url, json=query, headers=headers)
 print(r.status_code)
 print(r.json())
